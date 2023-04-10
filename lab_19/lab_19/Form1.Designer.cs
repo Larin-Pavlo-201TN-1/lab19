cc892fc9b38536d3383319002dd7b78bebdbdf99
Pavlo-Larin-201TN <98223565+Pavlo-Larin-201TN@users.noreply.github.com>
2023-04-10 08:39:45 +0300

    Add files via upload

---
+++ lab_19/lab_19/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace lab_19
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.arr_txt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.result1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.result2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // arr_txt
+            // 
+            this.arr_txt.Location = new System.Drawing.Point(12, 31);
+            this.arr_txt.Name = "arr_txt";
+            this.arr_txt.Size = new System.Drawing.Size(359, 22);
+            this.arr_txt.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введіть масив рядків";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "1 слово за алфавітом:";
+            // 
+            // result1
+            // 
+            this.result1.AutoSize = true;
+            this.result1.Location = new System.Drawing.Point(12, 89);
+            this.result1.Name = "result1";
+            this.result1.Size = new System.Drawing.Size(0, 16);
+            this.result1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(190, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Слова що мають дві букви d:";
+            // 
+            // result2
+            // 
+            this.result2.AutoSize = true;
+            this.result2.Location = new System.Drawing.Point(12, 135);
+            this.result2.Name = "result2";
+            this.result2.Size = new System.Drawing.Size(0, 16);
+            this.result2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(283, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 31);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Виконати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.result2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.result1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.arr_txt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox arr_txt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label result1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label result2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ lab_19/lab_19/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_19
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            result2.Text = "";
+            int count = 0;
+            char[] f = new char[3] { ' ', ',', ';' };
+            string[] arr = arr_txt.Text.Split(f, StringSplitOptions.RemoveEmptyEntries);
+            Array.Sort(arr);
+            if (arr.Length > 0)
+                result1.Text = arr[0];
+            else
+            {
+                result1.Text = "Масив не заповнений";
+            }
+            for(int i = 0; i < arr.Length; i++)
+            {
+                for (int j =  0; j < arr[i].Length; j++)
+                {
+                    if (arr[i][j] == 'd') {
+                        count++;
+                    }
+                }
+                if(count == 2)
+                {
+                    count = 0;
+                    result2.Text += arr[i] + "; ";
+                }
+            }
+        }
+    }
+}
